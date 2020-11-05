c367d32fa2fd982f433658f08a3d08865dfff75f
Stelyan Georgiev <stelyan.georgiev@paysafe.com>
2020-11-05 20:03:33 +0200

    BestCommitMessageSuperDetailed

--- mom.sol
+++ mom.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.17;
 contract SuperMom {
     address public firstBornAddress;
     address public secondBornAddress;
+    uint public totalEthereumSent;
     
     constructor(address firstBorn, address secondBorn) public {
         firstBornAddress = firstBorn;
@@ -11,5 +12,7 @@ contract SuperMom {
     function give() public payable {
         firstBornAddress.transfer(msg.value/2);
         secondBornAddress.transfer(msg.value/2);
+        totalEthereumSent+=msg.value;
     }
+    
 }
